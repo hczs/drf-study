825fe2c175f2a973250ffb5f23b9274664186d31
houcheng <houcheng@boco.com.cn>
2021-11-10 21:39:58 +0800

    :recycle: 重构视图类，使用视图集来重构之前的代码

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,14 +1,16 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from snippets import views
 
-urlpatterns = [
-    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
-    path('', views.api_root),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
-]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# Create a router and register our viewsets with it.
+# 创建一个router（路由器），并把视图集注册进去
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
+# 这个API所有的URL由router自动确定
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- snippets/views.py
+++ snippets/views.py
@@ -3,61 +3,47 @@ from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics
 from rest_framework import permissions
-from rest_framework.decorators import api_view
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework import renderers
+from rest_framework import viewsets
 
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format),
-    })
-
-
-class SnippetHighlight(generics.GenericAPIView):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    这个是自定义的一个视图，不用默认的api，而是用自己的实现
+    This viewset automatically provides `list` and `retrieve` actions.
+    这个视图集自动提供了查询所有和查询一个的方法
+    一个UserViewSet就做了之前users和userDetail两个类的活了
     """
-    queryset = Snippet.objects.all()
-    renderer_classes = [renderers.StaticHTMLRenderer]
-
-    def get(self, request, *args, **kwargs):
-        snippet = self.get_object()
-        return Response(snippet.highlighted)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
-class SnippetList(generics.ListCreateAPIView):
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    这个类自动提供list、create、retrieve、update、destroy方法
+    Additionally we also provide an extra `highlight` action.
+    此外，还会提供额外的highlight操作
+    这一个类，做了之前SnippetHighlight、SnippetList、SnippetDetail三个类做的活了
+    """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    # 添加permission_classes，做权限控制
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    # 添加permission_classes，做权限控制，定制化权限，仅允许snippet的owner进行编辑操作(IsOwnerOrReadOnly)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    # 请注意，我们还使用@action装饰器创建了一个名为 的自定义操作highlight。这个装饰可以用来添加不符合标准的任何自定义端点create/ update/delete风格。
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
     # create()我们的序列化程序的方法现在将传递一个额外的'owner'字段，以及来自请求的验证数据
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-    # 添加permission_classes，做权限控制，定制化权限，仅允许snippet的owner进行编辑操作(IsOwnerOrReadOnly)
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-
-
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
