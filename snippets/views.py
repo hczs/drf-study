322394463fb6e94e02a9ab306e0f6432cf69b5ea
houcheng <houcheng@boco.com.cn>
2021-11-10 21:11:59 +0800

    :sparkles: 添加分页，添加api_root，通过访问根目录查看所有API

--- snippets/views.py
+++ snippets/views.py
@@ -5,10 +5,34 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format),
+    })
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    """
+    这个是自定义的一个视图，不用默认的api，而是用自己的实现
+    """
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class SnippetList(generics.ListCreateAPIView):
