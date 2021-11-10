8930c574a4906246614b35da67d51f8c28c6012b
houcheng <houcheng@boco.com.cn>
2021-11-10 15:06:30 +0800

    :recycle: 版本2.2：使用generic class-based views

--- snippets/views.py
+++ snippets/views.py
@@ -2,49 +2,17 @@ from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework import mixins
 from rest_framework import generics
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
-    """
-    继承了mixins基类，来提供基础的list和create方法
-    继承generics.GenericAPIView，来构建视图
-    """
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
-    """
-    使用GenericAPIView类来提供核心功能
-    添加mixins提供retrieve、update、destroy方法
-    """
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
